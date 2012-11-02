3dc0824ef15540a45bfedb8dbf8f81bc44586451
osborn <>
2012-11-02 05:16:18 +0000

    removed old includes

--- lib/pcstubs/hisq_force_fnmat_d2.c
+++ lib/pcstubs/hisq_force_fnmat_d2.c
@@ -3,6 +3,6 @@
 #define QOP_Precision 'D'
 #define QOP_Nc 2
 
-#include "hisq_force_fnmat_p.c"
+//#include "hisq_force_fnmat_p.c"
 
 #include "hisq_force_fnmat2_p.c"
--- lib/pcstubs/hisq_force_fnmat_f3.c
+++ lib/pcstubs/hisq_force_fnmat_f3.c
@@ -1,6 +1,6 @@
 #define QDP_Precision 'F'
 #define QOP_Precision 'F'
 
-#include "hisq_force_fnmat_p.c"
+//#include "hisq_force_fnmat_p.c"
 
 #include "hisq_force_fnmat2_p.c"
