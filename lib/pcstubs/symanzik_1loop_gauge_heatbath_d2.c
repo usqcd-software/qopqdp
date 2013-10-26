0fff7fcec666ef6c548e6f4baa29f649b03c2409
osborn <>
2013-10-26 23:07:09 +0000

    added missing files

---
+++ lib/pcstubs/symanzik_1loop_gauge_heatbath_d2.c
@@ -0,0 +1,11 @@
+#define QOP_Precision 'D'
+#define QOP_Colors 2
+#define QOP_Nc 2
+#define QDP_Precision 'D'
+#define QDP_Colors 2
+#define QDP_Nc 2
+#define QLA_Precision 'D'
+#define QLA_Colors 2
+#define QLA_Nc 2
+
+#include "symanzik_1loop_gauge_heatbath_p.c"
